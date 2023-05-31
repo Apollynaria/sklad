3b439d61eb69ddb7452052ba6e4130e26d120ea1
Apollynaria <a.polly.narii@gmail.com>
2023-05-31 16:23:09 +0800

    v0.2

--- main.py
+++ main.py
@@ -8,24 +8,25 @@ k = 50  # количество итераций
 array_vec = []
 
 for i in range(k_vec):
-    vec = [0] * len(r)  # массив 0
-    array_r = r.copy()  # копируем изначальный массив
-    weight_vec = 0
+    vec = [0] * len(r)
+
+    # Получение случайного числа элементов для замены
+    num_ones = random.randint(1, len(r) - 1)  # ????
+
+    # Получение списка случайных уникальных индексов
+    indices = random.sample(range(len(r)), num_ones)
+
+    # Замена элементов по индексам на 1
+    weight = 0
     c = 0
-    random.shuffle(array_r)
-    for j in range(len(r)):
-        if not array_r:  # проверяем, есть ли еще элементы в списке
-            break
-        rnd_el = array_r.pop()
-        individual = r.index(rnd_el)  # выбор случайной особи из всех
-        if r[individual][0] + weight_vec <= max_w:
-            vec[individual] = 1
-            weight_vec += r[individual][0]
-            c += r[individual][1]
-    pair = [vec, c]
-    array_vec.append(pair)
+    for index in indices:
+        vec[index] = 1
+        weight += r[index][0]
+        c += r[index][1]
 
-sorted_array_vec = sorted(array_vec, key=lambda x: x[1])
+    data = [vec, weight, c]
+    array_vec.append(data)
+
+sorted_array_vec = sorted(array_vec, key=lambda x: x[2])
 print(*sorted_array_vec, sep="\n")
-
 
